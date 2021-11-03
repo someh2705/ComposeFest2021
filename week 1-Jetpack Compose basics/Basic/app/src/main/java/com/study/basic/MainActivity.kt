8ef11deeb4684d66a1bfb73db64cf7783df8b4ee
someh <someh2705@gmail.com>
2021-11-04 01:15:58 +0900

    feat: Styling and theming your app

--- week 1-Jetpack Compose basics/Basic/app/src/main/java/com/study/basic/MainActivity.kt
+++ week 1-Jetpack Compose basics/Basic/app/src/main/java/com/study/basic/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.study.basic
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -17,6 +18,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.study.basic.ui.theme.BasicTheme
@@ -93,7 +95,9 @@ private fun Greeting(name: String) {
                     .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
                 Text(text = "Hello, ")
-                Text(text = name)
+                Text(text = name, style = MaterialTheme.typography.h4.copy(
+                    fontWeight = FontWeight.ExtraBold
+                ))
             }
             OutlinedButton(onClick = { expanded = !expanded }) {
                 Text(text = "Show more")
@@ -103,10 +107,30 @@ private fun Greeting(name: String) {
     }
 }
 
-@Preview(showBackground = true, name = "Text preview", widthDp = 320)
+@Preview(
+    showBackground = false,
+    widthDp = 320,
+    heightDp = 320
+)
+@Composable
+private fun OnboardPreview() {
+    BasicTheme {
+        OnboardingScreen {
+
+        }
+    }
+}
+
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "Default Preview Dart"
+)
+@Preview(showBackground = true, widthDp = 320)
 @Composable
 private fun DefaultPreview() {
     BasicTheme {
-        MyApp(init = false)
+        MyApp(false)
     }
 }
