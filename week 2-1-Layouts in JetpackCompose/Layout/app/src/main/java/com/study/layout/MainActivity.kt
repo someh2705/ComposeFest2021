23ca42be7ffe582c5e99f63041b5a09eb4cf4ad9
someh <someh2705@gmail.com>
2021-11-12 22:54:01 +0900

    feat: PaddingModifier class

--- week 2-1-Layouts in JetpackCompose/Layout/app/src/main/java/com/study/layout/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/Layout/app/src/main/java/com/study/layout/MainActivity.kt
@@ -19,14 +19,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.AlignmentLine
-import androidx.compose.ui.layout.FirstBaseline
-import androidx.compose.ui.layout.Layout
-import androidx.compose.ui.layout.layout
+import androidx.compose.ui.layout.*
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.*
 import coil.compose.rememberImagePainter
 import com.study.layout.ui.theme.LayoutTheme
 import kotlinx.coroutines.launch
@@ -318,5 +314,33 @@ fun Chip(modifier: Modifier = Modifier, text: String) {
 fun ChipPreview() {
     LayoutTheme {
         Chip(text = "Hi there")
+    }
+}
+
+private class PaddingModifier(
+    val start: Dp = 0.dp,
+    val top: Dp = 0.dp,
+    val end: Dp = 0.dp,
+    val bottom: Dp = 0.dp,
+    val rtlAware: Boolean,
+) : LayoutModifier {
+    override fun MeasureScope.measure(
+        measurable: Measurable,
+        constraints: Constraints
+    ): MeasureResult {
+        val horizontal = start.roundToPx() + end.roundToPx()
+        val vertical = top.roundToPx() + bottom.roundToPx()
+
+        val placeable = measurable.measure(constraints.offset(-horizontal, -vertical))
+
+        val width = constraints.constrainWidth(placeable.width + horizontal)
+        val height = constraints.constrainHeight(placeable.height + vertical)
+        return layout(width, height) {
+            if (rtlAware) {
+                placeable.placeRelative(start.roundToPx(), top.roundToPx())
+            } else {
+                placeable.place(start.roundToPx(), top.roundToPx())
+            }
+        }
     }
 }
